c99ba6a0aeebf36660a7ae814bc8ed81ae2fff0c
MCauz <michaelhcausey@gmail.com>
2020-07-16 16:35:00 -0400

    need button js

--- src/index.js
+++ src/index.js
@@ -12,4 +12,49 @@ document.addEventListener("DOMContentLoaded", () => {
       toyFormContainer.style.display = "none";
     }
   });
-});
+  
+  let toyCollection = document.getElementById(`toy-collection`)
+  let url = "http://localhost:3000/toys"
+
+
+  let renderToys = function (toyArray){
+    toyArray.forEach(toyObj => {
+      let cards = document.createElement(`div`)
+        cards.className = `card`
+        cards.innerHTML = `<h2>${toyObj.name}</h2>
+        <img src=${toyObj.image} class="toy-avatar">
+        <p>Number of Likes: ${toyObj.likes}</p>
+        <button class="like-btn">Like <3</button>`
+        toyCollection.append(cards)
+    });
+  };
+
+  let toyForm = document.querySelector(`.add-toy-form`)
+    toyForm.addEventListener(`submit`, function (e){
+      e.preventDefault()
+      let imageInput = toyForm.querySelector(`input[name="image"]`)
+      let nameInput = toyForm.querySelector(`input[name="name"]`)
+      let newToy = {
+        "name": `${nameInput.value}`,
+        "image": `${imageInput.value}`,
+        "likes": 0
+      }
+      fetch(`${url}`, {
+        method: "POST",
+        headers: 
+        {
+          "Content-Type": "application/json",
+          Accept: "application/json"
+        },
+
+        body: JSON.stringify(newToy)
+      })
+      .then(response => response.json())
+      .then(data => console.log(data))
+      
+    });
+
+  fetch(url)
+  .then(response => response.json())
+  .then(results => renderToys(results))
+})
