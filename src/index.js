2bb577490cecbf2db91d6f019a24dfc609d096bd
estherkang14 <estherkang14@gmail.com>
2020-07-16 17:33:17 -0400

    started like button function

--- src/index.js
+++ src/index.js
@@ -23,11 +23,33 @@ document.addEventListener("DOMContentLoaded", () => {
         cards.className = `card`
         cards.innerHTML = `<h2>${toyObj.name}</h2>
         <img src=${toyObj.image} class="toy-avatar">
-        <p>Number of Likes: ${toyObj.likes}</p>
+        <p>Number of Likes: <span>${toyObj.likes}</span></p>
         <button class="like-btn">Like <3</button>`
         toyCollection.append(cards)
     });
   };
+
+  document.addEventListener("click", function(e){
+    let likeParentNode = e.target.parentNode
+    
+    if (e.target.className === "like-btn"){
+      let pTarget = likeParentNode.querySelector("p")
+      let likeSpan = pTarget.querySelector("span")
+      let newLikeNum = parseInt(likeSpan.innerText, 10) + 1 
+      console.log(newLikeNum)
+      // fetch(`${url}/[id goes here]`), {
+      //   method: "PATCH",
+      //   headers: 
+      //   {
+      //     "Content-Type": "application/json",
+      //     Accept: "application/json"
+      //   },
+      //   body: JSON.stringify({
+      //     "likes": <new number>
+      //   })
+      // }
+    }
+  })
 
   let toyForm = document.querySelector(`.add-toy-form`)
     toyForm.addEventListener(`submit`, function (e){
